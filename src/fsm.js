1e578564219f26559764a1d25d3ae639c43a3bba
Ken <10784731+kozeghong@users.noreply.github.com>
2017-12-21 15:18:35 +0800

    add shouldTransit

--- src/fsm.js
+++ src/fsm.js
@@ -1,47 +1,58 @@
 export default class FSM {
 
-    constructor () {
-        this.STATE = {
-            CLOSED: 'closed',
-            OPENED: 'opened',
-            LOCKED: 'locked'
-        }
-        this.ACTION = {
-            OPEN: 'open',
-            CLOSE: 'close',
-            LOCK: 'lock',
-            UNLOCK: 'unlock'
-        }
-        this.transitions = [
-            { from: this.STATE.CLOSED, action: this.ACTION.OPEN, to: this.STATE.OPENED },
-            { from: this.STATE.OPENED, action: this.ACTION.CLOSE, to: this.STATE.CLOSED },
-            { from: this.STATE.CLOSED, action: this.ACTION.LOCK, to: this.STATE.LOCKED },
-            { from: this.STATE.LOCKED, action: this.ACTION.UNLOCK, to: this.STATE.CLOSED }
-        ]
-        this.currentState = this.STATE.CLOSED
+  constructor () {
+    this.STATE = {
+      CLOSED: 'closed',
+      OPENED: 'opened',
+      LOCKED: 'locked'
     }
+    this.ACTION = {
+      OPEN: 'open',
+      CLOSE: 'close',
+      LOCK: 'lock',
+      UNLOCK: 'unlock'
+    }
+    this.transitions = [
+      { from: this.STATE.CLOSED, action: this.ACTION.OPEN, to: this.STATE.OPENED },
+      { from: this.STATE.OPENED, action: this.ACTION.CLOSE, to: this.STATE.CLOSED },
+      { from: this.STATE.CLOSED, action: this.ACTION.LOCK, to: this.STATE.LOCKED },
+      { from: this.STATE.LOCKED, action: this.ACTION.UNLOCK, to: this.STATE.CLOSED }
+    ]
+    this.currentState = this.STATE.CLOSED
+  }
 
-    transit (action, options = {}) {
-        const found = this.transitions.find(t => t.from === this.currentState && t.action === action)
-        if (!found) return
-        this.currentState = found.to
-        switch (this.currentState) {
-            case this.STATE.CLOSED:
-                return this.STATE.CLOSED
-                break
-            case this.STATE.OPENED:
-                return this.STATE.OPENED
-                break
-            case this.STATE.LOCKED:
-                return this.STATE.LOCKED
-                break
-            default:
-                break
-        }
+  transit (action, options = {}) {
+    const found = this.transitions.find(t => t.from === this.currentState && t.action === action)
+    if (!found)
+      return
+    if (!this.shouldTransit(found.to, options))
+      return
+    this.currentState = found.to
+    switch (this.currentState) {
+      case this.STATE.CLOSED:
+        return this.STATE.CLOSED
+        break
+      case this.STATE.OPENED:
+        return this.STATE.OPENED
+        break
+      case this.STATE.LOCKED:
+        return this.STATE.LOCKED
+        break
+      default:
+        break
     }
+  }
 
-    isState (state) {
-        return state === this.currentState
-    }
+  shouldTransit (nextState, options = {}) {
+    const funcName = `should${toCamelCase(`_${nextState}`)}`
+    return this[funcName] ? this[funcName](options) : true
+  }
+
+  isState (state) {
+    return state === this.currentState
+  }
 
 }
+
+const toCamelCase = str =>
+  str.replace(/^([A-Z])|[\s-_]+(\w)/g, (match, p1, p2, offset) => p2 ? p2.toUpperCase() : p1.toLowerCase())
--- src/test.js
+++ src/test.js
@@ -3,5 +3,5 @@ import FSM from './fsm'
 const fsm = new FSM()
 
 for (const action in fsm.ACTION) {
-    console.log(`I ${fsm.ACTION[action]} the door, now the door is:`, fsm.transit(fsm.ACTION[action]))
+  console.log(`I ${fsm.ACTION[action]} the door, now the door is:`, fsm.transit(fsm.ACTION[action]))
 }
